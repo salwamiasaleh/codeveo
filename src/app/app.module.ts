0c1279fe3041fe3f94e76753a77f5834a5c4f423
salwamiasaleh <salwamiasaleh@gmail.com>
2024-01-14 15:05:35 +0200

    Med

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,11 +15,13 @@ import { ServicesComponent } from './services/services.component';
 import { BannerComponent } from './banner/banner.component';
 import { OurWorkComponent } from './our-work/our-work.component';
 import { ClientsComponent } from './clients/clients.component';
-import { SlickCarouselModule } from 'ngx-slick-carousel';
+
 import { CardComponent } from './card/card.component';
 import { AccordionItemComponent } from './accordion-item/accordion-item.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { FooterComponent } from './footer/footer.component';
+
+
 const routes: Routes = [
  
   // other route configurations
@@ -43,11 +45,8 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
- 
     AppRoutingModule,
     RouterModule,
-    SlickCarouselModule,
-
     CarouselModule ,
     BrowserAnimationsModule,
   ],
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,7 +5,8 @@ import { OwlOptions } from 'ngx-owl-carousel-o';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+
 })
 export class HeaderComponent {
   constructor(){}
